18b0dfe697679ed6d1b70d7b1a8966bc4aaeabc0
0x0luke <lukejcw@icloud.com>
2019-05-12 01:14:39 +0100

    Removed all source files and just kept the ARM build script.

--- core/banner.go
+++
@@ -1,8 +0,0 @@
-package core
-
-const (
-	Name    = "aquatone"
-	Version = "1.5.0"
-	Author  = "Michael Henriksen"
-	Website = "https://github.com/michenriksen/aquatone"
-)
--- core/events.go
+++
@@ -1,8 +0,0 @@
-package core
-
-const (
-	Host          = "host"
-	URL           = "url"
-	URLResponsive = "url:responsive"
-	TCPPort       = "port:tcp"
-)
